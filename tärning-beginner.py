5548f5b7e6fa8a177fc8a22b57234c267c677a09
IronWar <raslor04@gmail.com>
2023-05-21 18:48:04 +0200

    Finished2

---
+++ tärning-beginner.py
@@ -0,0 +1,15 @@
+import random
+i = True
+
+while i:
+    tal = input("Gissa ett nummer mellan 1-6 ")
+
+    resultat = int(tal) == random.randint(1, 6)
+    print (resultat)
+
+    cont = input("Vill du fortsätta? ")
+
+    if cont == "Nej" or cont == "nej":
+        i = False
+
+    
